5d223fc6fbd34cf0e86c81e9f43168939e74f0dd
David T <ponzleblogging@gmail.com>
2011-03-31 23:41:51 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ponzle::Application.config.secret_token = 'b439766da89bf829d52f59706e8fad90decaaec9db63da27b8194233aa6dc1b98c4c9643f0300f2d77bd708c6a34f1491982344060e6fecc994927af28f0ab44'
